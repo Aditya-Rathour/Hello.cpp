5c59766d214d04282f0e9d03e1a50f82ab1c7f21
Aditya-Rathour <121443333+Aditya-Rathour@users.noreply.github.com>
2024-02-26 23:27:56 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Aditya Kumar"<<endl;
+    cout<<"21BCS3283"<<endl;
+    cout<<"21BCS-CC-621-A"<<endl;
+    cout<<"mark = 28/30?"<<endl;
+    return 0;
+}
